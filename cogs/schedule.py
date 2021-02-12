aaa5b65237e4672b769279bfbc073f63515f0829
Chris A <1drturtle@gmail.com>
2021-02-12 18:49:24 -0500

    change for live constants

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -15,6 +15,7 @@ class Schedule(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.task = None
+        self.running = True
         self.channel_id = SCHEDULE_CHANNEL_DEBUG if ENVIRONMENT == 'testing' else SCHEDULE_CHANNEL
 
     @commands.Cog.listener(name='on_ready')
@@ -23,19 +24,20 @@ class Schedule(commands.Cog):
         self.task.add_done_callback(self.task_error)
 
     async def every_minute(self):
-        while True:
+        while self.running:
             now = pendulum.now('America/New_York')
             next = now + pendulum.duration(minutes=1)
             next_msg = now.next(pendulum.FRIDAY).at(10)
-            log.info(f'it is {now.to_day_datetime_string()}. {self.bot.guilds}')
-            if now.day_of_week == pendulum.FRIDAY and now.hour == 17:
+            if now.day_of_week == pendulum.FRIDAY and now.hour == 10:
                 channel = self.bot.get_channel(self.channel_id)
                 if channel:
-                    log.debug(f'sending message to #{channel.name} at {now.to_day_datetime_string()}')
-                    # await channel.send('-')
+                    log.debug(f'sending message to #{channel.name} at {now.to_day_datetime_string()}. '
+                              f'next at {next_msg.to_day_datetime_string()}')
+                    await channel.send('<@&773895151008874518>, Please restock the Scrolls!',
+                                       allowed_mentions=discord.AllowedMentions(roles=True))
                 else:
                     log.error(f'could not find channel with id {self.channel_id}. skipping message.')
-            await discord.utils.sleep_until(next)
+            await discord.utils.sleep_until(next_msg)
 
     def task_error(self, task):
         if e := task.exception():
