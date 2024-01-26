7269cf1a55b3d53c74e4b986d36600582ba9d625
Chris A <1drturtle@gmail.com>
2024-01-25 22:12:36 -0500

    Update dm_queue.py

--- cogs/dm_queue.py
+++ cogs/dm_queue.py
@@ -267,7 +267,7 @@ class DMQueue(commands.Cog):
             gate = Group.from_dict(self.bot.get_guild(self.server_id), raw_data)
             gates.append(GateGroup(gate=gate, name=name, claimed=claimed))
 
-        gates = gates[-1::]
+        gates = gates[:10]
         gates.reverse()
         return gates
 
