dd8391a4a40470d3f6109920020022ae0844efc2
Chris A <1drturtle@gmail.com>
2024-04-08 22:05:37 -0400

    concat too long signups

--- ui/queue_admin_menu.py
+++ ui/queue_admin_menu.py
@@ -467,7 +467,9 @@ class DMSelector(disnake.ui.StringSelect):
 
         # options based on dm_queue_data
         options = [
-            (dm.nick or dm.display_name) + ": " + d_data.get("ranks")
+            (l := (dm.nick or dm.display_name))
+            + ": "
+            + d_data.get("ranks")[: 99 - len(l)]
             for dm, d_data in self.dms
         ]
         if not options:
